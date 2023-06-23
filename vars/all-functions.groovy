d9fc1a90aad31588c3b95d671b61296952824e71
AhmedKhamis9780 <ahmed.khamis9780@gmail.com>
2023-06-23 08:57:31 +0200

    again

--- vars/all-functions.groovy
+++ vars/all-functions.groovy
@@ -1,39 +1,32 @@
-def call() {
-    pipeline {
-        agent any
-        parameters {
-        
-            booleanParam(name:'project', defaultValue: true, description:'this paramater help you to know project name')
-            choice(name: 'namespace', choices:['dev','prod','stage'], description: '' ) 
-        }
 
-        stages {
-            stage('check') {
-                steps {
-                    echo "checking your code"
-                    echo "${params.namespace}"
-                
-                }
-            }
-
-            stage('test') {
-                when {
-                    expression{
-                        params.project == true 
+ def call(String namespace,boolean project){
+            stages {
+                stage('check') {
+                    steps {
+                        echo "checking your code"
+                        echo "${namespace}"
+                    
                     }
                 }
-                steps {
-                    echo "testing your app" 
-                }
-            }
-            
-            stage('deployment') {  
-                steps {
-                    echo "your code is deployed right now"
-                    echo "this build number $BUILD_NUMBER"
-                }
-            }    
-        }
 
-    }
-}    
+                stage('test') {
+                    when {
+                        expression{
+                            project == true 
+                        }
+                    }
+                    steps {
+                        echo "testing your app" 
+                    }
+                }
+                
+                stage('deployment') {  
+                    steps {
+                        echo "your code is deployed right now"
+                        echo "this build number $BUILD_NUMBER"
+                    }
+                }    
+            }
+
+        }
+       
